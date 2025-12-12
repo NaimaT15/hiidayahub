7dfef0566509b5879d25bc6f33bc363ccc91a501
NaimaT15 <naimatilahun77@gmail.com>
2025-12-12 05:10:14 -0800

    project setup

---
+++ src/main/java/com/hiidayahub/hiidayahub/HiidayahubApplication.java
@@ -0,0 +1,13 @@
+package com.hiidayahub.hiidayahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiidayahubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiidayahubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hiidayahub/hiidayahub/HiidayahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiidayahub.hiidayahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiidayahubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
